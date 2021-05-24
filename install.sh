6e0ab193030a8a98473883f863fbc3b439ca1393
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-05-24 17:43:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
 pip3 install smsactivateru
+echo OK!!
+apt install -y make python build-essential
+echo OK!!
 cp -r /root/shell-bot/sms.sh /bin/sms
+echo OK!!
+cp -r /root/shell-bot/telefon.py /bin/tel
+echo OK!!
+npm install
+echo OK!!
+node server.js
