d9abedc4e0e8b87894481bf3e0a7ca07aaff0985
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-05-24 22:41:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,24 @@
+apt update 
+echo OK!!  update
+sleep 2
+apt install npm -y 
+echo OK!! npm
+sleep 2
 pip3 install smsactivateru
-echo OK!!
+echo OK!!  smsactivateru
+sleep 2
 apt install -y make python build-essential
-echo OK!!
+echo OK!! make python build-essential
+sleep 2 
 cp -r /root/shell-bot/sms.sh /bin/sms
-echo OK!!
+echo OK!!  sms
+sleep 2
 cp -r /root/shell-bot/tel.sh /bin/tel
-echo OK!!
+echo OK!! tel
+sleep 2
 npm install
-echo OK!!
+echo OK!!  npm install
+sleep 2
+echo START SERVER CICADA3301
+sleep 4
 node server.js
